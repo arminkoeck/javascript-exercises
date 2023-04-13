1a8e77127934ba2360b6d63e2a0c018baaf67d5c
Armin Köck <armin.koeck@gmail.com>
2023-04-13 14:18:30 +0200

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let newTemp = (fahrenheit - 32) / (9/5);
+  return +(newTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let newTemp = celsius * (9/5) + 32;
+  return +(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
