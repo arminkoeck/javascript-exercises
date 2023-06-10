927eba5a0741d261fe0b2992dfbbddc070a9da45
Armin Köck <armin.koeck@gmail.com>
2023-06-10 17:39:23 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let arr = [1, 1];
+    let currentNumber = 2;
+    if (member < 1) return "OOPS";
+    for (i = 2; i < member; i++) {
+    arr[currentNumber] = arr[currentNumber - 1] + arr[currentNumber - 2];
+    currentNumber += 1;
+    };
+    return arr[member - 1];
 };
 
 // Do not edit below this line
