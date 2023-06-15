7e3633ec7a0582d8523894eda1cca1cd80914b38
Armin Köck <armin.koeck@gmail.com>
2023-06-15 21:14:57 +0200

    Failed attempt for exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,40 @@
-const caesar = function() {
 
+
+const caesar = function(word, number) {
+    let charCodeNumbers = [];
+    for (i = 0; i < word.length; i++) {
+        charCodeNumbers[i] = word.charCodeAt(i);
+    };
+    newCharCodeNumbers = charCodeNumbers.map(character => character + number);
+    let caesarArray = newCharCodeNumbers.map(character => String.fromCharCode(character));
+    let finalString = caesarArray.join('');
+    return finalString;
 };
+
+const caesar = function(word, number) {
+    return word
+        .split("");
+        .map(character => shift(character, number));
+        .join("");
+    
+    shift(character, number) {
+        let unicode = character.charCodeAt();
+        newUnicode = unicode.map(character => {
+            if ((character + number) > )
+            character + number
+        });
+    }
+}
+
+
+
+    };
+    newCharCodeNumbers = charCodeNumbers.map(character => character + number);
+    let caesarArray = newCharCodeNumbers.map(character => String.fromCharCode(character));
+    let finalString = caesarArray.join('');
+    return finalString;
+};
+
 
 // Do not edit below this line
 module.exports = caesar;
