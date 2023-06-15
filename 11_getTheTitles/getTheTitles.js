e48e14df08c72e4350991390a51487fb5ffe5454
Armin Köck <armin.koeck@gmail.com>
2023-06-15 11:57:52 +0200

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
 
+
+const getTheTitles = function(array) {
+    const newArray = array.map(array => array.title);
+    return newArray
 };
 
 // Do not edit below this line
