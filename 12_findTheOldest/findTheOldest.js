7ef3e4548c99565f57877b8c40995294dd7e48ad
Armin Köck <armin.koeck@gmail.com>
2023-06-15 19:30:05 +0200

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
 
+
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a,b) {
+        const firstPerson = getAge(a.yearOfBirth, a.yearOfDeath)
+        const nextPerson = getAge(b.yearOfBirth, b.yearOfDeath)
+        return firstPerson > nextPerson ? 1 : -1;
+    });
+    return oldest[oldest.length-1]
 };
+
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
